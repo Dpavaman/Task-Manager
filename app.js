d1a199d44cfe0eeade0937ab5fff5d5b5e7cadfb
Pavaman Dabeer <pavamandabeer@gmail.com>
2021-12-09 23:00:53 +0530

    Connect DB, handle Errors, handle invalid routes, and initialise the Project with an asynchronous DB connect

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const app = express();
 const tasks = require("./routes/tasks");
+const connectDB = require("./db/connect");
+const notFound = require("./middlewares/not-found");
+const errorHandlerMiddleware = require("./middlewares/error-handler");
+require("dotenv").config();
 
 //middleware
 app.use(express.json());
-
+app.use(errorHandlerMiddleware);
 //routes
 app.get("/", (req, res) => {
   res.send("Task Manager App");
@@ -12,6 +16,19 @@ app.get("/", (req, res) => {
 
 app.use("/api/v1/tasks", tasks);
 
-const port = 3000;
+app.use(notFound);
+const port = process.env.PORT || 3000;
 
-app.listen(port, console.log(`Server is Up and running on port ${port}...`));
+const initializeProject = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(
+      port,
+      console.log(`Server is Up and running on port ${port}...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+initializeProject();
