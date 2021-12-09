72d8fbd450df9881d23a8a8105dd9f19bab7dcca
Pavaman Dabeer <pavamandabeer@gmail.com>
2021-12-09 23:01:56 +0530

    functionalities for CRUD operations on data with DB

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,22 +1,49 @@
-const getAllTasks = (req, res) => {
-  res.send("All items from the file");
-};
+const asyncWrapper = require("../middlewares/async-wrapper");
+const Task = require("../models/Task");
 
-const createTask = (req, res) => {
-  res.send("Create Task");
-};
+const getAllTasks = asyncWrapper(async (req, res) => {
+  const tasks = await Task.find({});
+  res.status(200).json(tasks);
+});
 
-const getTask = (req, res) => {
-  res.send("Signle item from the file");
-};
+const createTask = asyncWrapper(async (req, res) => {
+  const newTask = await Task.create(req.body);
+  res.status(201).json({ newTask });
+});
 
-const updateTask = (req, res) => {
-  res.send("update one item");
-};
+const getTask = asyncWrapper(async (req, res) => {
+  const task = await Task.findOne({ _id: req.params.id });
+  if (!task) {
+    return res
+      .status(404)
+      .json({ message: `No task with id ${req.params.id} ` });
+  }
+  res.status(201).json({ message: task });
+});
 
-const deleteTask = (req, res) => {
-  res.send("delete one item");
-};
+const deleteTask = asyncWrapper(async (req, res) => {
+  const task = await Task.findOneAndDelete({ _id: req.params.id });
+  if (!task) {
+    return res
+      .status(404)
+      .json({ message: `No Task with id ${req.params.id}` });
+  }
+  res.status(200).json({ task });
+});
+
+const updateTask = asyncWrapper(async (req, res) => {
+  const task = await Task.findByIdAndUpdate({ _id: req.params.id }, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  if (!task) {
+    return res
+      .status(404)
+      .json({ message: `No Task with id ${req.params.id}` });
+  }
+  res.status(200).json({ task });
+});
 
 module.exports = {
   getAllTasks,
